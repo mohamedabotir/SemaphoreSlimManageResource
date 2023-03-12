6b30503a1c134cd847469c5393861936a8775c62
mohamedabotyear <mohamedabotyear@gmail.com>
2023-03-12 11:04:43 +0200

    Add project files.

---
+++ SemaphoreSlimManageResource/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace SemaphoreSlimManageResource
+{
+    internal class Program
+    {
+        public static SemaphoreSlim _resource = new SemaphoreSlim(1);
+       public static HttpClient _client= new HttpClient()
+       {
+           Timeout= TimeSpan.FromSeconds(5),
+       };
+        static async Task Main(string[] args)
+        {
+           Task.WaitAll(Program.ProcessWork().ToArray());
+        }
+       static async Task InvokeWork() {
+
+            try
+            {
+            await    _resource.WaitAsync();
+          var _request = await  _client.GetAsync("https://www.bing.com/");
+                Console.WriteLine(_request.StatusCode.ToString());
+                _resource.Release();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+       static  IEnumerable<Task> ProcessWork() {
+            for (int i = 0; i < 1000; i++)
+            {
+                yield return  InvokeWork();
+            }
+        }
+    }
+}
